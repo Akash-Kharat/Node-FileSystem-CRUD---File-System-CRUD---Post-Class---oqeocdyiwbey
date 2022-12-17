dc6f3bf63ae6f275676f0dec6f9449fbc17f76cc
MicroEngineer <61626411+Akash-Kharat@users.noreply.github.com>
2022-12-17 21:13:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,16 +13,9 @@ const myFileWriter = async (fileName, fileContent) => {
 const myFileReader = async (fileName) => {
 	try {
 		await fs.readFile(fileName, { encoding: 'utf8' }).then(a => console.log(a))
-	console.log("Ok")
-	
-// 	  try {
-		
-// 		await fs.readFile(fileName, "utf-8", (_err, data) => {
-// 		console.log(data);
-// 		return data;
-// 	  })
-// // >>>>>>> c9cb79e5063f742bce505e54115c616a48076c63
-	  } catch (err) {
+
+	  }
+	catch (err) {
 		console.log(err);
 	  }  
 }
